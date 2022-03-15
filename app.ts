67c6fcddae0f21ff8f3e4ddbba3e2f601f85fdaa
heng1025 <472263356@qq.com>
2022-03-15 16:51:30 +0800

    fix: app type error

--- app.ts
+++ app.ts
@@ -2,8 +2,9 @@ import config from "./config";
 
 const { AWTRIX_HOST } = config;
 
-App<IAppOption & { baseURL: string }>({
+type AppOptions<T> = WechatMiniprogram.App.Options<T>;
+
+App<AppOptions<{baseURL:string}>>({
   baseURL: `${AWTRIX_HOST}/api/v3`,
-  globalData: {},
   onLaunch() {},
 });
